75d3c3b85e303e5ada961278a7e9b8201859a021
Arthur Pinheiro <114116008+vuvu43@users.noreply.github.com>
2023-07-10 00:20:44 -0300

    readerCSV.py

--- readerCSV.py
+++ readerCSV.py
@@ -1,67 +1,93 @@
 import pandas as pd
 
 
-csvfile = pd.read_csv("BDPRojFinal\output.csv", nrows=1000).values.tolist()
 ordem = ['codlegislatura','datemissao','idedocumento','idecadastro','indtipodocumento',
          'nucarteiraparlamentar','nudeputadoid','nulegislatura','numano','numespecificacaosubcota',
          'numlote','nummes','numparcela','numressarcimento','numsubcota','sgpartido','sguf',
          'txtnomeparlamentar','txtcnpjcpf','txtdescricao','txtdescricaoespecificacao',
          'txtfornecedor','txtnumero','txtpassageiro','txttrecho','vlrdocumento','vlrglosa','vlrliquido','vlrrestituicao']
+controle = []
+lotecontrole = []
+partidocon = []
+legcon = []
+fornecedorcon = []
 
+sgpartido = ordem.index("sgpartido") 
+txtcnpjcpf = ordem.index("txtcnpjcpf")
+txtfornecedor = ordem.index("txtfornecedor")
+codlegislatura = ordem.index("codlegislatura")
+nulegislatura = ordem.index("nulegislatura")
+lote = ordem.index("numlote")
+nudeputadoid = ordem.index("nudeputadoid")
+sguf = ordem.index("sguf")
+txtnomeparlamentar = ordem.index("txtnomeparlamentar")
+idecadastro = ordem.index("idecadastro")
+nucarteiraparlamentar = ordem.index("nucarteiraparlamentar")
+inddoc = ordem.index("indtipodocumento") 
+txtnumero = ordem.index("txtnumero") 
+numsubcota = ordem.index("numsubcota") 
+txtdescricao = ordem.index("txtdescricao") 
+datemissao = ordem.index("datemissao") 
+vlrdocumento  = ordem.index("vlrdocumento")                                          
+vlrglosa = ordem.index("vlrglosa") 
+numparcela = ordem.index("numparcela") 
+numressarcimento = ordem.index("numressarcimento") 
+vlrrestituicao = ordem.index("vlrrestituicao") 
+nummes = ordem.index("nummes") 
+numano = ordem.index("numano") 
+txtpassageiro = ordem.index("txtpassageiro") 
+txttrecho = ordem.index("txttrecho") 
+id = -1
 
-sgpartido = ordem.index("sgpartido") + 1
-txtcnpjcpf = ordem.index("txtcnpjcpf")+ 1
-txtfornecedor = ordem.index("txtfornecedor")+ 1
-codlegislatura = ordem.index("codlegislatura")+ 1
-nulegislatura = ordem.index("nulegislatura")+ 1
-lote = ordem.index("numlote")+ 1
-nudeputadoid = ordem.index("nudeputadoid")+ 1
-sguf = ordem.index("sguf")+ 1
-txtnomeparlamentar = ordem.index("txtnomeparlamentar")+ 1
-idecadastro = ordem.index("idecadastro")+ 1
-nucarteiraparlamentar = ordem.index("nucarteiraparlamentar")+ 1
-inddoc = ordem.index("indtipodocumento") + 1
-txtnumero = ordem.index("txtnumero") + 1
-numsubcota = ordem.index("numsubcota") + 1
-txtdescricao = ordem.index("txtdescricao") + 1
-datemissao = ordem.index("datemissao") + 1
-vlrdocumento  = ordem.index("vlrdocumento") + 1                                         
-vlrglosa = ordem.index("vlrglosa") + 1
-numparcela = ordem.index("numparcela") + 1
-numressarcimento = ordem.index("numressarcimento") + 1
-vlrrestituicao = ordem.index("vlrrestituicao") + 1
-nummes = ordem.index("nummes") + 1
-numano = ordem.index("numano") + 1
-txtpassageiro = ordem.index("txtpassageiro") + 1
-txttrecho = ordem.index("txttrecho") + 1
-
-
-with open("BDPRojFinal\inserirPartido.sql", 'w') as insert:
+for i in range(100):
+    csvfile = pd.read_csv("BDPRojFinal\cota-parlamentar.csv", skiprows=i*1000, nrows=1000).values.tolist()
+    tam = len(csvfile[0])
     for linha in csvfile:
-        insert.write(f"INSERT INTO Partido(sgPartido) VALUES ('{linha[sgpartido]}');\n")
+        '''
+        #usar se necessário
+        for i in range(tam):
+            if isinstance(linha[i], str) and "'" in linha[i]:
+                linha[i] = linha[i].replace("'", "")
+        
+        #usar se necessário
+        '''
+        
+        #inserir subcota no arquivo
+        with open("BDPRojFinal\inserirSubcotaAtualizado.sql", 'a') as insert:
+            id += 1
+            insert.write(f"INSERT INTO Subcota_Documento(idSubcota, txtNumero, numSubCota, txtDescricaoEspecificacao, datEmissao,"+
+                        "vlrDocumento, vlrGlosa, numParcela, numRessarcimento, vlrRestituicao, numMes, numAno, txtPassageiro, txtTrecho, numEspecificacaoSubCota, indTipoDocumento, txtcnpjcpf, numLote, nuDeputadoid)\n"+
+                            f"VALUES ({id},'{linha[txtnumero]}', {linha[numsubcota]}, '{linha[txtdescricao]}', '{linha[datemissao]}', {linha[vlrdocumento]}, {linha[vlrglosa]}, {linha[numparcela]}, {linha[numressarcimento]}, {linha[vlrrestituicao]}, {linha[nummes]}, {linha[numano]}, '{linha[txtpassageiro]}', '{linha[txttrecho]}', {linha[numsubcota]}, {linha[inddoc]}, '{linha[txtcnpjcpf]}', {linha[lote]}, {linha[nudeputadoid]});\n\n")
 
-with open("BDPRojFinal\inserirFornecedor.sql", 'w') as insert:
-    for linha in csvfile:
-        insert.write(f"INSERT INTO Fornecedor(txtcnpjcpg, txtfornecedor) VALUES ('{linha[txtcnpjcpf]}', '{linha[txtfornecedor]}');\n")
+        #inserir fornecedores no arquivo
+        with open("BDPRojFinal\inserirFornecedorAtualizado.sql", 'a') as insert:
+            if linha[txtcnpjcpf] not in fornecedorcon:
+                fornecedorcon.append(linha[txtcnpjcpf])
+                insert.write(f"INSERT INTO Fornecedor(txtcnpjcpf, txtfornecedor) VALUES ('{linha[txtcnpjcpf]}', '{linha[txtfornecedor]}');\n")
+        
+        #inserir Lote no arquivo
+        with open("BDPRojFinal\inserirLote.sql", 'a') as insert:
+            insert.write(f"INSERT INTO Lote(numLote) VALUES ({linha[txtcnpjcpf]});\n")
 
-with open("BDPRojFinal\inserirLote.sql", 'w') as insert:
-    for linha in csvfile:
-        insert.write(f"INSERT INTO Lote(numLote) VALUES ({linha[lote]});\n")
+        #inserir legislatura no arquivo
+        with open("BDPRojFinal\inserirLegislatura.sql", 'a') as insert:
+            if linha[codlegislatura] not in legcon:
+                legcon.append(linha[codlegislatura])
+                insert.write(f"INSERT INTO Legislatura(codLegislatura, nuLegislatura) VALUES ({linha[codlegislatura]}, {linha[nulegislatura]});\n")
+        
+        #inserir partidos no arquivo
+        tempPartido = linha[sgpartido]
+        if tempPartido not in partidocon:
+            partidocon.append(tempPartido)
+            with open("BDPRojFinal\inserirPartido.sql", 'a') as insert:
+                insert.write(f"INSERT INTO Partido(sgPartido) VALUES ('{linha[sgpartido]}');\n")
 
-with open("BDPRojFinal\inserirLegislatura.sql", 'w') as insert:
-    for linha in csvfile:
-        insert.write(f"INSERT INTO Legislatura(codLegislatura, nuLegislatura) VALUES ({linha[codlegislatura]}, {linha[nulegislatura]});\n")
-
-with open("BDPRojFinal\inserirParlamentar.sql", 'w') as insert:
-    for linha in csvfile:
-        insert.write(f"INSERT INTO Parlamentar(nuDeputadoid, sgUF, txtnomeparlamentar, ideCadastro, nuCarteiraParlamentar, sgPartido, codLegislatura)\n" +
-                     f"VALUES ({linha[nudeputadoid]}, '{linha[sguf]}', '{linha[txtnomeparlamentar]}', {linha[idecadastro]}, {linha[nucarteiraparlamentar]}, '{linha[sgpartido]}', {linha[codlegislatura]});\n")
-
-with open("BDPRojFinal\inserirSubcota.sql", 'w') as insert:
-    for linha in csvfile:
-        insert.write(f"INSERT INTO Subcota_Documento(txtNumero, numSubCota, txtDescricaoEspecificacao, datEmissao,"+
-                         "vlrDocumento, vlrGlosa, numParcela, numRessarcimento, vlrRestituicao, numMes, numAno, txtPassageiro, txtTrecho, numEspecificacaoSubCota, indTipoDocumento, txtcnpjcpf, numLote, nuDeputadoid)\n"+
-                         f"VALUES ('{linha[txtnumero]}', {linha[numsubcota]}, '{linha[txtdescricao]}', '{linha[datemissao]}', {linha[vlrdocumento]}, {linha[vlrglosa]}, {linha[numparcela]}, {linha[numressarcimento]}, {linha[vlrrestituicao]}, {linha[nummes]}, {linha[numano]}, '{linha[txtpassageiro]}', '{linha[txttrecho]}', {linha[numsubcota]}, {linha[inddoc]}, '{linha[txtcnpjcpf]}', {linha[lote]}, {linha[nudeputadoid]});\n\n")
-
-                                                                                                                                                                                                                                                                
-            
+        #inserir deputados no arquivo
+        depid = linha[nudeputadoid]
+        if depid not in controle:
+            controle.append(depid)
+            with open("BDPRojFinal\inserirParlamentar.sql", 'a') as insert:
+                insert.write(f"INSERT INTO Parlamentar(nuDeputadoid, sgUF, txtnomeparlamentar, ideCadastro, nuCarteiraParlamentar, sgPartido, codLegislatura)\n" +
+                            f"VALUES ({linha[nudeputadoid]}, '{linha[sguf]}', '{linha[txtnomeparlamentar]}', {linha[idecadastro]}, {linha[nucarteiraparlamentar]}, '{linha[sgpartido]}', {linha[codlegislatura]});\n")
+                                                                                                                                                                                                                                                           
+                
